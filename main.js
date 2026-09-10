c2c6a5f3adf6e4b5725247d3b326d5cd8228ba97
Mohamed Khaled <mohamedkhaled65556@gmail.com>
2026-09-10 13:32:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ const historyTransaction = () => {
 let inputs = document.querySelectorAll(".otp-input");
 inputs.forEach((input, index) => {
   input.addEventListener("input", () => {
-    if (index < inputs.length - 1) {
+    if (input.value && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
   });
